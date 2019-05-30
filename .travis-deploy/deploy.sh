21a86f6067083dcb8f6440e651f8a88770ba794f
Max Hooton <max.hooton@auden.co.uk>
2019-05-30 15:39:46 +0100

    remove cruft

--- .travis-deploy/deploy.sh
+++ .travis-deploy/deploy.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-
-echo "TravisPullRequest: $TRAVIS_PULL_REQUEST"
-echo "TravisBranch: $TRAVIS_BRANCH"
-
 if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     if [ "$TRAVIS_BRANCH" == "master" ]; then
         bash .travis-deploy/ecs.sh -c $CLUSTER -n $APP -i "$AWS_ECR/$APP:latest" -r $AWS_DB_REGION -t 240
