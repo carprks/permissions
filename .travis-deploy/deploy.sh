f4351bfd2cf7b7bd8211cecd548213fa9f1614d9
Max Hooton <max.hooton@auden.co.uk>
2019-05-30 15:03:27 +0100

    see if this works

--- .travis-deploy/deploy.sh
+++ .travis-deploy/deploy.sh
@@ -5,6 +5,6 @@ echo "TravisBranch: $TRAVIS_BRANCH"
 
 if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     if [ "$TRAVIS_BRANCH" == "master" ]; then
-        .travis-deploy/ecs.sh -c $CLUSTER -n $APP -i "$AWS_ECR/$APP:latest" -r $AWS_DB_REGION -t 240
+        bash .travis-deploy/ecs.sh -c $CLUSTER -n $APP -i "$AWS_ECR/$APP:latest" -r $AWS_DB_REGION -t 240
     fi
 fi
